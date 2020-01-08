a625c519938781853195b32fbd3cd3c7b757af53
Payal Shah <payalshah.13b@gmail.com>
2020-01-07 20:32:47 -0600

    Adding web api.

---
+++ ListifyWebAPI/ListifyWebAPI/Controllers/ListifyController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ListifyWebAPI.Controllers
+{
+    public class ListifyController : ApiController
+    {
+        [HttpGet]
+        [Route("listify/{begin}/{end}/{index}")]
+        public int GetSequenceIndex(int begin, int end, int index)
+        {
+            var list = GetRangeArray(begin, end);
+            var val = list[50];
+            return val;
+        }
+
+        public IEnumerable<int> GetSequence(int start, int end)
+        {
+            var seq = Enumerable.Range(start, end);
+            return seq;
+        }
+
+        public IList<int> GetRangeArray(int start, int end)
+        {
+            var arr = GetSequence(start, end).ToArray();
+            return arr;
+        }
+        
+    }
+}
